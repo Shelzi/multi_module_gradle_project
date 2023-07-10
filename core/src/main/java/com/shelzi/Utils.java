e6f3dd0760126fe42c199ea232a6cbf096b7d2ad
Shelzi <nemant38@gmail.com>
2023-07-10 16:08:07 +0200

    change repository source

--- core/src/main/java/com/shelzi/Utils.java
+++ core/src/main/java/com/shelzi/Utils.java
@@ -1,11 +1,10 @@
 package com.shelzi;
 
-import static com.shelzi.StringUtils.isPositiveNumber;
 
 public class Utils {
     static boolean isAllPositiveNumbers(String... str) {
         for (String s : str) {
-            if (!isPositiveNumber(s)) return false;
+            if (!StringUtils.isPositiveNumber(s)) return false;
         }
         return true;
     }
